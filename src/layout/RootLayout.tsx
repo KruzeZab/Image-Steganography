1c80399b47a63be2416041012dd0455005229df7
kruzezab <kruzezab4@gmail.com>
2023-03-04 23:53:42 +0545

    create navbar

---
+++ src/layout/RootLayout.tsx
@@ -0,0 +1,11 @@
+import Header from "./header/Header";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default RootLayout;
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+import RootLayout from "../layout/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<RootLayout />}></Route>)
+);
+
+export default router;
