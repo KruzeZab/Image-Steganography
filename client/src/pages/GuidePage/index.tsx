4789263d255d73afd8397f0b3dd3a1abd57280a2
Kruze Zab <kruzezab4@gmail.com>
2023-06-15 06:32:08 +0545

    add guide page

--- client/src/pages/GuidePage/index.tsx
+++ client/src/pages/GuidePage/index.tsx
@@ -1,187 +1,108 @@
-import { Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  ListItem,
+  OrderedList,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
 
 const GuidePage = () => {
   return (
-    <>
-      <Container maxW={"8xl"}>
-        <Heading as="h1" pb={5} size="lg" textAlign={"center"}>
-          How it works?
-        </Heading>
+    <Container maxW={"8xl"} pt={20}>
+      <Heading as="h2" pb={3} size="md">
+        How it works?
+      </Heading>
+      <Text size="md" mb={4}>
+        Image steganography is a technique that involves hiding secret
+        information within an image to ensure its covert transmission or
+        storage. The goal is to make the presence of the hidden data
+        undetectable to anyone who may intercept or analyze the image.
+        Here&apos;s overview of how this app works:
+      </Text>
 
-        <Text size="md">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nemo
-          assumenda tempore, perspiciatis, autem ad tenetur recusandae dolore a
-          repellendus facilis cupiditate, impedit aperiam officia incidunt
-          quaerat iste tempora natus! Quos dignissimos fuga in architecto sunt
-          quaerat assumenda nesciunt eaque, explicabo labore iusto facilis
-          accusamus, quia rerum saepe molestias esse repellat perferendis
-          ratione voluptatum doloribus, officiis veniam quis? Tempore, dolore.
-          Earum facere ipsa nam soluta exercitationem rerum! At vero harum, fuga
-          magni velit doloremque totam labore ullam culpa aut odio architecto
-          atque, cumque veniam dolorem tempora. Quia autem quibusdam quae. Optio
-          esse repellendus, nemo et nulla obcaecati delectus magni, natus odit
-          veniam voluptates id ex ea dolor maiores quisquam facere facilis
-          nesciunt eius ipsam in qui? Quia temporibus velit fuga? Minus aperiam
-          labore ad earum numquam porro nostrum sunt deserunt sequi ipsam
-          inventore, facilis accusantium error nesciunt quaerat! Magnam,
-          quisquam ipsam sit dolorum quod asperiores est veritatis error
-          expedita iusto. Perferendis, alias? Unde, explicabo sed. Porro numquam
-          iste repudiandae vitae, vel, error laboriosam libero aliquam doloribus
-          dolores tempora architecto ratione dolorum nulla. Laudantium quisquam
-          aspernatur, eligendi quibusdam delectus ex id. Reprehenderit quasi sit
-          similique nesciunt. Perspiciatis ducimus aut fugit vitae
-          reprehenderit? Laborum, atque. Facilis magnam quis voluptate
-          praesentium, quo sunt ipsam reprehenderit ea, natus, voluptatum rem
-          maiores at similique molestiae? Aliquid tempora ex voluptas accusamus
-          et excepturi voluptate fugiat, cum autem ad nisi deleniti fuga aperiam
-          rem quibusdam culpa minus nobis facilis impedit id facere nam, iure
-          iusto? Accusamus, pariatur! Optio sed alias odio nam praesentium
-          dolorem sint delectus molestiae eos consequatur pariatur aspernatur
-          placeat voluptatem doloribus obcaecati nemo ipsam voluptas voluptate,
-          suscipit cum. Recusandae nam quam vitae non eos. Doloremque unde hic
-          sed odit illum! Dolor optio, aliquam, iure voluptatem exercitationem
-          harum maxime ipsa eius consectetur praesentium id vel eos corporis
-          nemo ratione aspernatur obcaecati ad fugiat magni? Dignissimos! Sed
-          corporis cupiditate iste sapiente illo debitis unde fuga aspernatur
-          ipsa rem inventore perferendis quod, aliquid ex fugiat officiis
-          distinctio doloremque voluptatem, tempora placeat vero assumenda nobis
-          totam nisi! Natus. Dolore, laboriosam dolores voluptates quo,
-          cupiditate asperiores odit recusandae dolorem unde tempora, alias
-          veritatis. Aliquid at fugiat neque accusamus quaerat. Similique
-          expedita voluptatibus quaerat placeat aliquam velit nam possimus
-          quibusdam. Voluptas impedit vitae eius similique illum quae ipsum
-          excepturi molestias repellendus atque? Tenetur, quam repellat autem
-          modi dolore quasi enim commodi eos natus possimus omnis cum, quod
-          porro. Mollitia, sint! Architecto excepturi sunt, labore quas
-          voluptates voluptatibus exercitationem repellat ratione necessitatibus
-          ad tempora praesentium quam, ducimus assumenda numquam harum rem,
-          ipsum atque blanditiis quasi! Incidunt provident quaerat nostrum eos
-          necessitatibus! Ut ipsum, ea reiciendis a iusto voluptate fugit
-          accusantium eaque odit, soluta voluptas. Totam reprehenderit nobis eos
-          et repellat perferendis, autem, aliquid ratione, corrupti provident
-          cupiditate? Doloremque hic reprehenderit consequuntur. Culpa accusamus
-          repellendus nam ea nihil laudantium. Iusto perferendis sed veniam!
-          Provident, minus eaque quisquam reprehenderit tenetur atque totam quas
-          laboriosam eligendi neque dolor nemo, dolorum nisi architecto sit
-          ratione! Tenetur, iusto suscipit facere fugiat consequatur vitae
-          deleniti neque, esse est consequuntur minus. Provident sequi,
-          reiciendis quisquam, asperiores aliquid eos necessitatibus molestias
-          est modi tenetur in, voluptate rem pariatur sint! Culpa asperiores
-          iure amet cumque, maiores, error consequatur tempore quod quidem dicta
-          facilis aut repudiandae quas cum laboriosam sequi illo laborum quae!
-          Modi, at. Omnis excepturi eveniet maxime porro voluptate. Itaque nisi
-          culpa excepturi iste illo sint molestiae ipsam, veniam minima illum!
-          Deleniti veniam repudiandae recusandae atque perspiciatis a doloribus
-          quaerat quae iste natus fuga, culpa, sed quos rem magni. Accusamus
-          molestiae optio pariatur. Cum possimus totam quaerat quos quo in dolor
-          a placeat, sed ad eius aspernatur corporis optio tempora architecto
-          natus recusandae earum, sapiente saepe voluptates nisi rerum. Porro
-          sequi vero accusamus rerum vel, eos minus necessitatibus architecto
-          quaerat atque harum numquam. Dolor eligendi tempore id laudantium?
-          Eum, dignissimos vero velit enim delectus ut totam nobis esse
-          nesciunt. Modi illum, cupiditate atque minus error quae similique amet
-          porro corporis neque commodi? Maiores aliquam quas quidem dignissimos
-          praesentium veniam molestias. Ipsam aspernatur quibusdam quia!
-          Temporibus nobis cum minus tenetur! Reprehenderit aspernatur rem iusto
-          animi harum nulla suscipit rerum ratione ut itaque magni laudantium
-          facere doloremque sequi autem maxime, inventore voluptate. Odio,
-          accusamus consectetur in soluta quaerat consequatur quos voluptate.
-          Illum tempore nobis deleniti numquam repellendus aliquid, nesciunt
-          corporis! Cumque nobis beatae voluptas eum facere. Doloremque, ipsa
-          dolorum? Quis, ipsum recusandae accusantium vitae reiciendis dolor
-          minus nemo sed mollitia quaerat? Illum ullam perferendis perspiciatis
-          nostrum in voluptate. Quibusdam quam asperiores dolorem earum odio.
-          Distinctio temporibus ipsum aliquam ipsa, doloremque possimus
-          molestias illum nihil consectetur aliquid, dolore unde ipsam commodi!
-          Accusamus! Expedita provident facere commodi cum ipsum sapiente,
-          consequatur voluptas voluptatibus aut, quia ad ducimus qui autem!
-          Maiores natus, aperiam, suscipit mollitia nemo culpa repellendus
-          provident repudiandae eum odio quidem quod? Doloremque incidunt
-          officiis id veniam commodi sequi sed laboriosam est quaerat quas optio
-          ratione dolor, repudiandae delectus amet illum asperiores rerum eaque
-          quasi tempore aliquam dolores consequuntur. A, blanditiis dolores.
-          Maiores obcaecati similique amet itaque esse quisquam, dolore,
-          provident unde neque ducimus totam rem optio iure et incidunt, quo
-          autem corporis eligendi laborum? Deleniti molestias impedit eveniet,
-          veritatis numquam laboriosam. Eius hic natus sit voluptas cum. Cumque
-          esse neque perspiciatis nisi quisquam aliquam voluptate nobis quis
-          porro a eos repellat, distinctio dolorum velit sunt debitis nesciunt
-          ratione accusantium ab qui. Omnis recusandae magni, repellendus vitae
-          quae earum? Est labore distinctio provident, quas pariatur beatae
-          nostrum cupiditate possimus, praesentium aperiam veritatis aliquam
-          fugiat iste totam officia soluta facilis amet accusamus ipsum.
-          Asperiores veritatis animi rem saepe voluptatum, magnam optio expedita
-          nemo cumque ratione pariatur incidunt soluta, repellat, obcaecati
-          iusto itaque magni dolores sit! Similique, non quidem? Dignissimos
-          amet id quod deleniti. Quisquam velit quos vel nulla incidunt in
-          voluptatibus consectetur rem, qui sapiente magni magnam ducimus
-          eligendi veritatis voluptates amet, reprehenderit exercitationem
-          facere animi natus iusto voluptas! Laborum assumenda nisi labore?
-          Molestiae dolorem incidunt rerum laboriosam at tenetur facere non
-          voluptatem quia corporis aspernatur possimus soluta hic quisquam cum
-          ducimus sit cumque debitis accusamus molestias quam voluptates illo,
-          consectetur explicabo? Excepturi. Dolorum amet reprehenderit tempore
-          facere repellat. Illo eius sit maxime eaque vitae? Iure architecto
-          nam, magnam cupiditate in esse reprehenderit delectus tempora? Sint
-          exercitationem placeat asperiores animi expedita ad deserunt? Quo
-          facere vitae, inventore dolor ipsum commodi consectetur praesentium ad
-          impedit sapiente? Itaque natus mollitia, est consectetur cupiditate
-          excepturi eius, dolor at odio dignissimos quae quasi maxime ex nobis
-          ullam! Vel, sequi reiciendis quia, repellat quam natus maiores
-          corrupti aspernatur tenetur repudiandae assumenda cupiditate iure
-          quaerat deleniti voluptatibus hic sint cumque exercitationem commodi
-          veritatis! Deleniti nesciunt nam debitis voluptatibus mollitia? Omnis
-          dignissimos velit sint rerum voluptate assumenda porro quos ullam
-          natus eum modi ea similique eveniet iste quis nesciunt vero deserunt
-          ipsa voluptatum, totam ratione dolores expedita consequuntur enim!
-          Mollitia. Repellat culpa blanditiis voluptatum aspernatur iste sequi
-          officia, fugit veritatis, possimus iure, cumque odio consectetur qui
-          itaque eligendi velit facere recusandae eum fuga necessitatibus amet
-          laboriosam. Praesentium accusantium quasi atque. Ipsum, magni ad
-          quidem impedit in sapiente ipsam consequatur ea amet quas eligendi
-          aliquam quod corporis maiores, ullam unde nulla dolores tempora
-          quaerat non! Quae sunt rem dolor vel perspiciatis! Rerum nisi enim
-          quis, incidunt nostrum doloremque fuga architecto perspiciatis
-          explicabo dignissimos non itaque fugit ad, facere distinctio beatae
-          eligendi earum, eaque nesciunt! Autem velit eveniet ipsam repellendus,
-          eius porro. Praesentium obcaecati porro, aliquid fugit maiores officia
-          vitae id perspiciatis dolor temporibus laborum corrupti, accusantium
-          aut sit architecto magnam reiciendis consequuntur consectetur!
-          Laboriosam omnis maxime tempore quisquam fugiat repudiandae possimus?
-          Voluptates dolor amet vitae sequi odit, eaque, iure, beatae aspernatur
-          cum pariatur exercitationem dicta sint velit ipsam excepturi. Laborum
-          labore commodi et nostrum animi! Nobis repellat hic soluta sapiente
-          iure. Accusantium velit illum quas porro rerum tenetur aspernatur.
-          Repudiandae dolorum eum, eos nam in nemo. Rem dolores voluptates
-          aliquam, consequatur aspernatur voluptatum suscipit qui atque id,
-          autem esse sit est. Perspiciatis amet debitis dolore, exercitationem
-          autem neque repellendus similique culpa nihil. Dignissimos, commodi
-          maxime alias quasi suscipit distinctio, sed exercitationem deserunt
-          sapiente, repellendus asperiores iusto! Eos ut dignissimos iste
-          veniam. Obcaecati, totam perferendis consequuntur maiores doloremque
-          porro nam sed debitis, aliquid accusamus placeat minima quae mollitia
-          ut sunt perspiciatis? Voluptatum modi nesciunt, temporibus accusamus
-          neque voluptates quibusdam quas est officia! Minus quos nobis placeat
-          soluta, ratione quisquam vel expedita excepturi, at, distinctio facere
-          consequuntur eveniet sequi. Commodi quidem eveniet deleniti ea
-          exercitationem eius, esse veritatis a possimus. Error, dignissimos
-          impedit? Neque, sint ipsam accusamus nisi dicta quidem repellendus, ab
-          nemo eos fuga veniam qui cupiditate praesentium. Est quos illo libero?
-          Rerum ipsam possimus veniam ratione nostrum aliquam reiciendis
-          perspiciatis consequuntur? Itaque explicabo, sequi rem odio
-          reprehenderit soluta fuga magnam earum nemo nesciunt labore, alias
-          culpa quo consequuntur sed beatae. Placeat natus repellat nulla? Unde
-          veritatis nobis perferendis eveniet atque aspernatur. Libero, eligendi
-          incidunt natus, quasi consequatur aut non aspernatur nulla, sed quam
-          minus eaque inventore cum mollitia ab corrupti quos at. Dolore
-          necessitatibus voluptatem sit dicta expedita quas suscipit! Voluptas.
-          Voluptate, officia totam. Veritatis, delectus asperiores nam quisquam
-          ipsam molestiae ipsum ea. Saepe quos, optio et, maxime delectus,
-          consequuntur velit dignissimos harum vel odit inventore ut assumenda
-          doloribus suscipit laudantium.
-        </Text>
-      </Container>
-    </>
+      <OrderedList>
+        <ListItem mb={4}>
+          <strong>Image Selection: </strong>
+          The process begins by selecting an appropriate cover image that will
+          be used to hide the secret message. The cover image should ideally be
+          large enough to accommodate the additional data without visibly
+          altering its appearance.
+        </ListItem>
+        <ListItem mb={4}>
+          <strong>Secret Message Encoding: </strong>
+          The secret message, which can be any form of digital data, such as
+          text, files, or other images, needs to be encoded before it can be
+          embedded within the cover image. Encoding methods may vary depending
+          on the chosen steganography algorithm.
+        </ListItem>
+        <ListItem mb={4}>
+          <strong>Steganography Algorithms:</strong>
+          There are various steganography algorithms available, each with its
+          own approach to hiding data within an image. Some common techniques
+          include:
+          <UnorderedList>
+            <ListItem mb={4}>
+              <strong>Least Significant Bit (LSB) Insertion: </strong>
+              This method involves replacing the least significant bit of each
+              pixel in the cover image with a bit from the secret message. Since
+              the LSB alteration is minimal, the change is often imperceptible
+              to the human eye.
+            </ListItem>
+            <ListItem mb={4}>
+              <strong>Spread Spectrum Technique: </strong>
+              This approach involves distributing the secret data across
+              multiple pixels in the cover image, making it harder to detect any
+              alterations. It uses mathematical transformations and frequency
+              domain analysis to embed the data.
+            </ListItem>
+            <ListItem mb={4}>
+              <strong>Transform Domain Techniques: </strong>
+              hese methods leverage mathematical transformations such as
+              Discrete Cosine Transform (DCT) or Discrete Fourier Transform
+              (DFT) to hide the secret data within the transformed coefficients
+              of the cover image.
+            </ListItem>
+          </UnorderedList>
+        </ListItem>
+        <ListItem mb={4}>
+          <strong>Embedding the Secret Message: </strong>
+          Once the secret message is encoded and the steganography algorithm is
+          selected, the embedding process begins. It typically involves the
+          following steps:
+          <UnorderedList>
+            <ListItem mb={4}>
+              Break the secret message into smaller chunks or packets.
+            </ListItem>
+            <ListItem mb={4}>
+              Determine the specific locations within the cover image where the
+              data will be inserted based on the steganography algorithm.
+            </ListItem>
+            <ListItem mb={4}>
+              Embed the data by modifying the pixel values or their attributes
+              in the cover image according to the chosen algorithm.
+            </ListItem>
+          </UnorderedList>
+        </ListItem>
+
+        <ListItem>
+          <strong>Extraction of the Secret Message</strong>
+          To retrieve the hidden data from the steganographic image, the
+          following steps are generally followed:
+          <UnorderedList>
+            <ListItem mb={4}>Locate the steganographic image.</ListItem>
+            <ListItem mb={4}>
+              Apply the reverse steganography algorithm to extract the embedded
+              data from the image.
+            </ListItem>
+            <ListItem mb={4}>
+              Reassemble the extracted data packets to reconstruct the original
+              secret message.
+            </ListItem>
+          </UnorderedList>
+        </ListItem>
+      </OrderedList>
+    </Container>
   );
 };
 
